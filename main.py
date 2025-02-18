b08fa5eb58930a873fa482974e5ccad2b117c335
ThobiasKH <thobiashoivik@gmail.com>
2025-02-18 11:47:41 +0100

    Changed dihedral group notation so that it uses D_2n instead of D_n

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from CayleyGraph import CayleyGraph
 import Groups
 
 # D4 Group
-d4Elements, d4Mult, d4Gen = Groups.DihedralGroup(4)
+d8Elements, d8Mult, d8Gen = Groups.DihedralGroup(4)
 
-D4_CGraph = CayleyGraph(d4Elements, d4Mult, d4Gen)
-D4_CGraph.build_graph()
-D4_CGraph.draw(layout="circular", save_as="D4_Graph.png")
+D8_CGraph = CayleyGraph(d8Elements, d8Mult, d8Gen)
+D8_CGraph.build_graph()
+D8_CGraph.draw(layout="circular", save_as="D8_Graph.png")
 
 # Integers mod 5 under addition :)
 z5Elements, z5Mult, z5Gen = Groups.CyclicGroup(5)
